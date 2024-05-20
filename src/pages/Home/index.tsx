c322abc5eb528af0ded3b11a077e62c8acdf314e
Horquidia <horquidia.a.lima@gmail.com>
2024-05-20 14:49:38 -0300

    Validando formulários

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,25 @@
 import { Play } from "phosphor-react";
 import {HomeContainer, FormContainer, CountdownContainer, Separator, StartCountdonwButton, TaskInput,MinutesAmountInput} from './styles'
 import { useForm } from "react-hook-form";
+import { zodResolver} from '@hookform/resolvers/zod'
+import * as zod from 'zod'
+
+const newCycleFormValidationSchema = zod.object({
+    task: zod.string().min(1, 'Informe a tarefa'),
+    minutesAmount: zod.number().min(5).max(60, 'O intervalo precisa ser de no maximo 60 minutos'),
+})
 
 export function Home(){
 
-    const {register, handleSubmit, watch} = useForm();
+    const {register, handleSubmit, watch, formState} = useForm({
+        resolver: zodResolver(newCycleFormValidationSchema),
+    });
 
     function handleCreateNewCycle(data: any){
-
+        console.log(data)
     }
+
+    console.log(formState.errors)
 
     const task = watch('task')
     const isSubmitDisabled = !task
@@ -39,7 +50,7 @@ export function Home(){
                         step={5} 
                         min={5} 
                         max={60}
-                        {...register('minutesAmount'), {valueAsNumber: true}}
+                        {...register('minutesAmount', {valueAsNumber: true})}
                     />
 
                     <span>minutos.</span>
