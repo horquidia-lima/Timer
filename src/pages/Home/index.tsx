5ee33ef88b252d7a1467bb1b415aad0886b5bdf9
Horquidia <horquidia.a.lima@gmail.com>
2024-05-20 15:01:28 -0300

    TypeScript no formulário

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -9,17 +9,26 @@ const newCycleFormValidationSchema = zod.object({
     minutesAmount: zod.number().min(5).max(60, 'O intervalo precisa ser de no maximo 60 minutos'),
 })
 
+/*interface NewCycleFormData {
+    task: string
+    minutesAmount: number
+}*/
+
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
+
 export function Home(){
 
-    const {register, handleSubmit, watch, formState} = useForm({
+    const {register, handleSubmit, watch} = useForm<NewCycleFormData>({
         resolver: zodResolver(newCycleFormValidationSchema),
+        defaultValues: {
+            task: '',
+            minutesAmount: 0,
+        }
     });
 
-    function handleCreateNewCycle(data: any){
+    function handleCreateNewCycle(data: NewCycleFormData){
         console.log(data)
     }
-
-    console.log(formState.errors)
 
     const task = watch('task')
     const isSubmitDisabled = !task
